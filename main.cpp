63acd01e535943ba63a924904766e7318bb649e6
Filipe Cattoni <filipecattoni@gmail.com>
2019-11-25 09:15:52 -0300

    terminado consulta na main

--- main.cpp
+++ main.cpp
@@ -22,6 +22,45 @@ void print_game(Game g) {
     cout << "-------------------------------" << endl;
 }
 
+Game read_game() {
+    Game g;
+    cout << "Digite o ID: " << endl;
+    cin >> g.id;
+    cout << "Digite o Nome: " << endl;
+    cin >> g.name;
+    cout << "Digite o Subtitulo: " << endl;
+    cin >> g.subtitle;
+    cout << "Digite a Avaliação média: " << endl;
+    cin >> g.avgRate;
+    cout << "Digite o Número de avaliações: " << endl;
+    cin >> g.countRate;
+    cout << "Digite o Preço: " << endl;
+    cin >> g.price;
+    cout << "Digite as Compras: " << endl;
+    cin >> g.purchases;
+    cout << "Digite a Descrição: " << endl;
+    cin >> g.description;
+    cout << "Digite o Desenvolvedor: " << endl;
+    cin >> g.developer;
+    cout << "Digite a Idade: " << endl;
+    cin >> g.age;
+    cout << "Digite as Línguas: " << endl;
+    cin >> g.languages;
+    cout << "Digite o Tamanho: " << endl;
+    cin >> g.size;
+    cout << "Digite o Gênero principal: " << endl;
+    cin >> g.primaryGenre;
+    cout << "Digite os Gêneros: " << endl;
+    cin >> g.genres;
+    cout << "Digite a Data de lançamento: " << endl;
+    cin >> g.originalRelease;
+    cout << "Digite a Data de atualização: " << endl;
+    cin >> g.lastRelease;
+    cout << "Digite o Ranqueamento: " << endl;
+    cin >> g.rank;
+    return g;
+}
+
 int main () {
 
     FILE *fp, *fi;
@@ -85,13 +124,45 @@ int main () {
 
             } else if (submenu == 3) {
 
+                cout << "Digite o nome: " << endl;
+                getline(cin, line);
+                while (line.length()==0) { getline(cin, line); }
+                strcpy(name, line.c_str());
+                fi = fopen("index-name.bin", "rb");
+                i = str_get_index(name, 0, fi);
+                fclose(fi);
+
+                if (i == -1) {
+                    cout << "Nome não encontrado. " << endl;
+                } else {
+
+                    fp = fopen("games.bin", "rb");
+                    fseek(fp, sizeof(Game)*i, SEEK_SET);
+                    fread(&temp, sizeof(Game), 1, fp);
+                    print_game(temp);
+                    fclose(fp);
+
+                }
+
             } else {
                 cout << "Opção inválida. " << endl << endl;
             }
 
         } else if (menu == 2) {
 
+            // temp = read_game();
+            // int ii;
+            // fp = fopen("games.bin", "rb");
+            // fseek(fp, 0, SEEK_END);
+            // ii = ftell(fp) / sizeof(Game);
+            // fclose(fp);
 
+            // fp = fopen("games.bin", "wb");
+            // fwrite(&temp, sizeof(Game), 1, fp);
+            // fclose(fp);
+
+            // NUM_Index ni; STR_Index si;
+            // ni.i = ii;
 
         } else {
             cout << "Opção inválida. " << endl << endl;
